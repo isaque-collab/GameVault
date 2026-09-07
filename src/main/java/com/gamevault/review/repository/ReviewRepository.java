7e9d938e6777f7934c767531624ac927f9aa0796
Isaque Cunha <costaisaque716@gmail.com>
2026-09-07 18:19:25 -0300

    feat: adiciona camada Repository de review

--- src/main/java/com/gamevault/review/repository/ReviewRepository.java
+++ src/main/java/com/gamevault/review/repository/ReviewRepository.java
@@ -1,4 +1,7 @@
 package com.gamevault.review.repository;
 
-public interface ReviewRepository {
+import com.gamevault.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
