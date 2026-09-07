c75d053eb96837e3897360fb4424c84c8d275760
Isaque Cunha <costaisaque716@gmail.com>
2026-09-07 18:15:52 -0300

    feat: adiciona camada Repository de favorite

--- src/main/java/com/gamevault/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/gamevault/favorite/repository/FavoriteRepository.java
@@ -1,4 +1,7 @@
 package com.gamevault.favorite.repository;
 
-public interface FavoriteRepository {
+import com.gamevault.favorite.entity.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 }
