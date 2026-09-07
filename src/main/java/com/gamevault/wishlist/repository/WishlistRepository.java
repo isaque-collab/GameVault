49748ff4aa11dd538a0fcc53e743bf17c7e2fe0b
Isaque Cunha <costaisaque716@gmail.com>
2026-09-07 18:17:40 -0300

    feat: adiciona camada Repository de wishlist

---
+++ src/main/java/com/gamevault/wishlist/repository/WishlistRepository.java
@@ -0,0 +1,7 @@
+package com.gamevault.wishlist.repository;
+
+import com.gamevault.wishlist.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+}
