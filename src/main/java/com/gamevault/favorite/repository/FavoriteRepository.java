9b637528eb974a09bec4d70cd4b0d447946fe8dd
Isaque Cunha <costaisaque716@gmail.com>
2026-09-08 15:06:12 -0300

    feat: implementa regras de negócio de favoritos

--- src/main/java/com/gamevault/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/gamevault/favorite/repository/FavoriteRepository.java
@@ -3,5 +3,14 @@ package com.gamevault.favorite.repository;
 import com.gamevault.favorite.entity.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+    boolean existsByUserIdAndRawgGameId(Long userId, Long rawgGameId);
+
+    Optional<Favorite> findByUserIdAndRawgGameId(Long userId, Long rawgGameId);
+
+    List<Favorite> findAllByUserId(Long userId);
 }
