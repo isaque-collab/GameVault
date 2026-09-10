140cd38135dc695415c058c8a670716f84e197ad
Isaque Cunha <costaisaque716@gmail.com>
2026-09-09 22:12:58 -0300

    test(usuario): adiciona testes de unicidade do usuário

--- src/test/java/com/gamevault/user/repository/UsuarioRepositoryIntegrationTest.java
+++ src/test/java/com/gamevault/user/repository/UsuarioRepositoryIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.gamevault.user.repository;
 
-
 import com.gamevault.user.entity.Usuario;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
@@ -17,7 +16,7 @@ class UsuarioRepositoryIntegrationTest {
     private UsuarioRepository usuarioRepository;
 
     @Test
-    void deveSalvarEBuscarUsuarioPorId(){
+    void deveSalvarEBuscarUsuarioPorId() {
         Usuario usuario = new Usuario();
         usuario.setName("Usuario Teste");
         usuario.setUsername("usuario_teste");
@@ -34,6 +33,56 @@ class UsuarioRepositoryIntegrationTest {
                 () -> assertEquals("Usuario Teste", usuarioEncontrado.getName()),
                 () -> assertEquals("usuario_teste", usuarioEncontrado.getUsername()),
                 () -> assertEquals("usuario.teste@gamevault.test", usuarioEncontrado.getEmail())
+        );
+    }
+
+    @Test
+    void deveIdentificarUsernameJaCadastrado() {
+        Usuario usuario = new Usuario();
+        usuario.setName("Usuario Teste");
+        usuario.setUsername("usuario_existente");
+        usuario.setEmail("usuario.username@gamevault.test");
+        usuario.setPassword("test-password-hash");
+
+        usuarioRepository.saveAndFlush(usuario);
+
+        assertTrue(
+                usuarioRepository.existsByUsername("usuario_existente")
+        );
+    }
+
+    @Test
+    void deveIdentificarEmailJaCadastrado() {
+        Usuario usuario = new Usuario();
+        usuario.setName("Usuario Teste");
+        usuario.setUsername("usuario_email");
+        usuario.setEmail("usuario.existente@gamevault.test");
+        usuario.setPassword("test-password-hash");
+
+        usuarioRepository.saveAndFlush(usuario);
+
+        assertTrue(
+                usuarioRepository.existsByEmail(
+                        "usuario.existente@gamevault.test"
+                )
+        );
+    }
+
+    @Test
+    void deveRetornarFalsoParaUsernameNaoCadastrado() {
+        assertFalse(
+                usuarioRepository.existsByUsername(
+                        "username_inexistente"
+                )
+        );
+    }
+
+    @Test
+    void deveRetornarFalsoParaEmailNaoCadastrado() {
+        assertFalse(
+                usuarioRepository.existsByEmail(
+                        "email.inexistente@gamevault.test"
+                )
         );
     }
 }
