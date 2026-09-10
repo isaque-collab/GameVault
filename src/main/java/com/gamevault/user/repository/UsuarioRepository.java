4b9ffcb933d7318095c32fe012c2d190a4e76aa1
Isaque Cunha <costaisaque716@gmail.com>
2026-09-09 22:12:31 -0300

    feat(usuario): adiciona consultas de unicidade

--- src/main/java/com/gamevault/user/repository/UsuarioRepository.java
+++ src/main/java/com/gamevault/user/repository/UsuarioRepository.java
@@ -4,4 +4,8 @@ import com.gamevault.user.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
