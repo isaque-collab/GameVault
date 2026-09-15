0fe5c0f6ac2695fe9e8f22a67d8715c4135d96bf
Isaque Cunha <costaisaque716@gmail.com>
2026-09-14 21:27:35 -0300

    feat: adiciona endpoint público de busca de jogos

---
+++ src/main/java/com/gamevault/jogo/controller/JogoController.java
@@ -0,0 +1,47 @@
+package com.gamevault.jogo.controller;
+
+import com.gamevault.jogo.dto.BuscaJogosResposta;
+import com.gamevault.jogo.service.JogoService;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jogos")
+public class JogoController {
+
+    private final JogoService jogoService;
+
+    public JogoController(JogoService jogoService) {
+        this.jogoService = jogoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<BuscaJogosResposta> buscarJogos(
+            @RequestParam(name = "nome")
+            @NotBlank(message = "O nome do jogo é obrigatório")
+            String nome,
+
+            @RequestParam(
+                    name = "pagina",
+                    defaultValue = "1"
+            )
+            @Min(
+                    value = 1,
+                    message = "A página deve ser maior ou igual a 1"
+            )
+            int pagina
+    ) {
+        BuscaJogosResposta resposta =
+                jogoService.buscarJogosPorNome(
+                        nome,
+                        pagina
+                );
+
+        return ResponseEntity.ok(resposta);
+    }
+}
--- src/main/java/com/gamevault/shared/exception/TratadorGlobalExcecoes.java
+++ src/main/java/com/gamevault/shared/exception/TratadorGlobalExcecoes.java
@@ -6,6 +6,8 @@ import com.gamevault.favorito.exception.FavoritoJaExisteException;
 import com.gamevault.favorito.exception.FavoritoNaoEncontradoException;
 import com.gamevault.listadesejos.exception.ItemListaDesejosJaExisteException;
 import com.gamevault.listadesejos.exception.ItemListaDesejosNaoEncontradoException;
+import com.gamevault.rawg.exception.RawgApiKeyNaoConfiguradaException;
+import com.gamevault.rawg.exception.RawgIntegracaoException;
 import com.gamevault.user.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -109,6 +111,34 @@ public class TratadorGlobalExcecoes {
         );
 
         problema.setTitle("Senha inválida");
+
+        return problema;
+    }
+
+    @ExceptionHandler(RawgIntegracaoException.class)
+    public ProblemDetail tratarFalhaIntegracaoRawg(
+            RawgIntegracaoException exception
+    ) {
+        ProblemDetail problema = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_GATEWAY,
+                exception.getMessage()
+        );
+
+        problema.setTitle("Falha na integração com a RAWG");
+
+        return problema;
+    }
+
+    @ExceptionHandler(RawgApiKeyNaoConfiguradaException.class)
+    public ProblemDetail tratarApiKeyRawgNaoConfigurada(
+            RawgApiKeyNaoConfiguradaException exception
+    ) {
+        ProblemDetail problema = ProblemDetail.forStatusAndDetail(
+                HttpStatus.SERVICE_UNAVAILABLE,
+                exception.getMessage()
+        );
+
+        problema.setTitle("Integração RAWG indisponível");
 
         return problema;
     }
